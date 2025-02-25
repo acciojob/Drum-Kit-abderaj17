7a6dbd326efaf6223b974c53684cb75337577200
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-25 22:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-//your JS code here. If required.
-const keys = document.querySelectorAll('.key');
-
 function playSound(e) {
-   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
-	if(!audio) return;
-
-	audio.currentTime = 0;
-	audio.play();
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  
+  if (!audio) return; // Stop the function if no audio element associated with the key
+  
+  key.classList.add('playing'); // Add a class to the key
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
 }
 
-window.addEventListener('keydown', playSound);
+// Remove the 'playing' class after a delay
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // Skip it if it's not a transform
+  this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
