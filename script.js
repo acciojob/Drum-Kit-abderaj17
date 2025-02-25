6282263c027aafcf93ff15808547078d321371c3
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-25 21:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const keys = document.querySelectorAll('.key');
+
+function playSound(e) {
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+	if(!audio) return;
+
+	audio.currentTime = 0;
+	audio.play();
+}
+
+window.addEventListener('keydown', playSound);
